074097f7020cf3f4023fe44e230df8f3b9080e47
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-01-12 10:53:35 +0400

    something's wrong, will come back to fix

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,117 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "The posts information could not be retrieved" });
+        })
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The post information could not be retrieved'
+        })
+    }
+})
+
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        Post.insert(req.body)
+            .then(postID => {
+                Post.findById(postID.id)
+                    .then(post => {
+                        res.status(201).json(post);
+                    })
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({ message: "There was an error while saving the post to the database" })
+            })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        const postSearch = await Post.findById(id);
+        if (!postSearch) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        } else {
+            Post.update(id, { title, contents })
+            .then(count => {
+                if (count === 1) {
+                    Post.findById(id)
+                        .then(post => {
+                            res.status(200).json(post);
+                        })
+                } else {
+                    res.status(500).json({ message: "The post information could not be modified" });
+                }
+            })    
+        }
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(foundPost => {
+            if (!foundPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                Post.remove(id)
+                    .then(count => {
+                        if (count === 1) {
+                            res.status(200).json(foundPost);
+                        } else {
+                            res.status(500).json({ message: "The post could not be removed" });
+                        }
+                    })
+            }
+        })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(foundPost => {
+            if (!foundPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                Post.findPostComments(id)
+                    .then(comments => {
+                        res.status(200).json(comments);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({ message: "The comments information could not be retrieved" })
+                    })
+            }
+        })
+})
+
+module.exports = router;
